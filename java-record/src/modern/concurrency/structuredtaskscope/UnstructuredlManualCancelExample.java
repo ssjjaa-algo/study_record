09b9599b889a67cf71eee771ad30e730849a68e0
김성재 <tjdwo6269@naver.com>
2026-04-27 23:54:18 +0900

    Refactor: 이름 변경

--- java-record/src/modern/concurrency/structuredtaskscope/UnstructuredlManualCancelExample.java
+++ java-record/src/modern/concurrency/structuredtaskscope/UnstructuredlManualCancelExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
-public class UnstructuredFastFailManualCancelExample {
+public class UnstructuredlManualCancelExample {
 
     public static void main(String[] args) throws Exception {
         try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
